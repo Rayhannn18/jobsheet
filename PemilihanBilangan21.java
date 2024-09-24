57aaf72d8fce5e6c1e8c23783c94f15ba2c67fb1
lakyulakyu <martieszhidan@gmail.com>
2024-09-24 15:41:52 +0700

    Pemilihan Switch-case

--- PemilihanBilangan21.java
+++ PemilihanBilangan21.java
@@ -8,12 +8,6 @@ public class PemilihanBilangan21 {
         System.out.print("masukkan angka :");
         int number = input.nextInt();
 
-        // if (number % 2 == 0) {
-        // System.out.println("Angka " + number + " Adalah angka genap");
-        // } else {
-        // System.out.println("Angka " + number + " Adalah angka ganjil");
-        // }
-
         String hasil = (number % 2 == 0) ? " adalah angka genap" : " adalah angka ganjil";
         System.out.println("Angka " + number + hasil);
         input.close();
---
+++ PemilihanSwitch21.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class PemilihanSwitch21 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double number1, number2, result = 0;
+        char operator;
+
+        System.out.print("Masukkan angka 1 : ");
+        number1 = input.nextDouble();
+        System.out.print("Masukkan angka 2 : ");
+        number2 = input.nextDouble();
+        System.out.print("Masukkan operator (+ - * :) : ");
+        operator = input.next().charAt(0);
+
+        switch (operator) {
+        case '+':
+        result = number1 + number2;
+        break;
+        case '-':
+        result = number1 - number2;
+        break;
+        case '*':
+        result = number1 * number2;
+        break;
+        case '/':
+        result = number1 / number2;
+        break;
+        default:
+        System.out.println("Masukan operator yang benar !");
+        }
+   
+        input.close();
+
+        if (operator == '-' || operator == '*' || operator == '/' || operator == '+') {
+            System.out.println(number1 + operator + number2 + " = " + result);
+            System.out.println(number1 + " " + operator + " " + number2 + " = " + result);
+        }
+
+    }
+
+}
