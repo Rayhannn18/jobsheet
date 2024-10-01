0a8022030335c63e21dac5d5bc551441fd11d5fb
Rayhannn18 <rayhangiri24@gmail.com>
2024-10-02 04:29:08 +0700

    test

--- Pemilihan2Percobaan3_14.java
+++ Pemilihan2Percobaan3_14.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Pemilihan2Percobaan314 {
+public class Pemilihan2Percobaan3_14 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String kategori;
