e5bbd051257a4fa1cf3b1a196bc5e24331220ddc
Rayhannn18 <rayhangiri24@gmail.com>
2024-10-08 12:25:33 +0700

    test

--- SiakadFor14.java
+++ SiakadFor14.java
@@ -4,6 +4,8 @@ public class SiakadFor14 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100;
+        int JumlahLulus = 0, JumlahTidakLulus = 0;
+        final double Batal_Kelulusan = 60.0; 
 
         for (int i = 1; i <= 10; i++  ) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ":");
@@ -14,9 +16,15 @@ public class SiakadFor14 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai >= Batal_Kelulusan) {
+                JumlahLulus++;
+            } else {
+                JumlahTidakLulus++;
+            }
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
-
+        System.out.println("Mahasiswa yang Lulus: " + JumlahLulus);
+        System.out.println("Mahasiswa yang tidak Lulus: " + JumlahTidakLulus);
     }
 }
