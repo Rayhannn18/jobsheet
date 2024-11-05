cc2d42e178ab3451435c2768b06fb37b7d0198f3
Rayhannn18 <rayhangiri24@gmail.com>
2024-11-05 12:37:45 +0700

    test

---
+++ Jobsheet 1/MyFirstJava14.java
@@ -0,0 +1,12 @@
+public class MyFirstJava14 {
+    public static void main(String[] args) {
+        System.out.println("***************************");
+        System.out.println("***** IDENTITAS DIRI ******");
+        System.out.println("***************************");
+        System.out.println("Nama    : Rayhan Giri Putra");
+        System.out.println("Absen   : 14");
+        System.out.println("Nim     : 244107060122");
+        System.out.println("Jurusan : Teknologi Informasi");
+        System.out.println("Prodi   : D-IV Sistem Informasi Bisnis");
+    }
+}
